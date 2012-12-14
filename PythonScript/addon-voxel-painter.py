fd8d9608250d6d5af5baf9a111966fe43ff51326
Luke Frisken <l.frisken@gmail.com>
2012-12-15 00:44:56 +1100

    Started the actual voxel editing operator, with selection example

--- PythonScript/addon-voxel-painter.py
+++ PythonScript/addon-voxel-painter.py
@@ -3,6 +3,7 @@ from bpy.props import StringProperty, BoolProperty, IntProperty, FloatProperty, 
                           FloatVectorProperty, EnumProperty, PointerProperty
 from bpy.types import Operator
 from mathutils import Vector
+from bpy_extras import view3d_utils
 
 def get_active(context):
     return context.scene.objects.active
@@ -94,30 +95,6 @@ class VoxelArray(object):
         return self.obj.children.__getitem__(index)
 
 
-class CreateVoxelsOperator(Operator):
-    """Operator to create and enable voxels on an empty"""
-
-    bl_idname = "object.create_voxels"
-    bl_label = "Create Voxels"
-    bl_options = {'UNDO'}
-
-    def execute(self, context):
-        obj = context.object
-        obj.vox_empty.created = True
-        va = VoxelArray(obj, context)
-        va.new_vox(Vector((1, 2, 3)))
-
-        del va
-
-
-        return {'FINISHED'}
-
-    @classmethod
-    def poll(cls, context):
-        ob = context.active_object
-        return ob is not None and ob.type == 'EMPTY'
-
-
 class VoxelEmpty_props(bpy.types.PropertyGroup):
     """This class stores all the overall properties for the voxel array"""
     created = BoolProperty(
@@ -202,6 +179,126 @@ class VoxelMesh_obj_prop(bpy.types.Panel):
         row.prop(obj, "name")
 
 
+
+
+
+class CreateVoxelsOperator(Operator):
+    """Operator to create and enable voxels on an empty"""
+
+    bl_idname = "object.create_voxels"
+    bl_label = "Create Voxels"
+    bl_options = {'UNDO'}
+
+    def execute(self, context):
+        obj = context.object
+        obj.vox_empty.created = True
+        va = VoxelArray(obj, context)
+        va.new_vox(Vector((1, 2, 3)))
+
+        del va
+
+
+        return {'FINISHED'}
+
+    @classmethod
+    def poll(cls, context):
+        ob = context.active_object
+        return ob is not None and ob.type == 'EMPTY'
+
+
+class EditVoxelsOperator(bpy.types.Operator):
+    """Modal object selection with a ray cast"""
+    bl_idname = "view3d.edit_voxels"
+    bl_label = "Voxel Editor"
+
+    def modal(self, context, event):
+        if event.type in {'MIDDLEMOUSE', 'WHEELUPMOUSE', 'WHEELDOWNMOUSE'}:
+            # allow navigation
+            return {'PASS_THROUGH'}
+        elif event.type == 'LEFTMOUSE':
+            main(context, event)
+            return {'RUNNING_MODAL'}
+        elif event.type in {'RIGHTMOUSE', 'ESC'}:
+            return {'CANCELLED'}
+
+        return {'RUNNING_MODAL'}
+
+    def invoke(self, context, event):
+        if context.space_data.type == 'VIEW_3D':
+            context.window_manager.modal_handler_add(self)
+            return {'RUNNING_MODAL'}
+        else:
+            self.report({'WARNING'}, "Active space must be a View3d")
+            return {'CANCELLED'}
+
+
+def main(context, event, ray_max=10000.0):
+    """Run this function on left mouse, execute the ray cast"""
+    # get the context arguments
+    scene = context.scene
+    region = context.region
+    rv3d = context.region_data
+    coord = event.mouse_region_x, event.mouse_region_y
+
+    # get the ray from the viewport and mouse
+    view_vector = view3d_utils.region_2d_to_vector_3d(region, rv3d, coord)
+    ray_origin = view3d_utils.region_2d_to_origin_3d(region, rv3d, coord)
+    ray_target = ray_origin + (view_vector * ray_max)
+
+    #scene.cursor_location = ray_target
+
+    def visible_objects_and_duplis():
+        """Loop over (object, matrix) pairs (mesh only)"""
+
+        for obj in context.visible_objects:
+            if obj.type == 'MESH':
+                yield (obj, obj.matrix_world.copy())
+
+            if obj.dupli_type != 'NONE':
+                obj.dupli_list_create(scene)
+                for dob in obj.dupli_list:
+                    obj_dupli = dob.object
+                    if obj_dupli.type == 'MESH':
+                        yield (obj_dupli, dob.matrix.copy())
+
+            obj.dupli_list_clear()
+
+    def obj_ray_cast(obj, matrix):
+        """Wrapper for ray casting that moves the ray into object space"""
+
+        # get the ray relative to the object
+        matrix_inv = matrix.inverted()
+        ray_origin_obj = matrix_inv * ray_origin
+        ray_target_obj = matrix_inv * ray_target
+
+        # cast the ray
+        hit, normal, face_index = obj.ray_cast(ray_origin_obj, ray_target_obj)
+
+        if face_index != -1:
+            return hit, normal, face_index
+        else:
+            return None, None, None
+
+    # cast rays and find the closest object
+    best_length_squared = ray_max * ray_max
+    best_obj = None
+
+    for obj, matrix in visible_objects_and_duplis():
+        if obj.type == 'MESH':
+            hit, normal, face_index = obj_ray_cast(obj, matrix)
+            if hit is not None:
+                length_squared = (hit - ray_origin).length_squared
+                if length_squared < best_length_squared:
+                    best_length_squared = length_squared
+                    best_obj = obj
+
+    # now we have the object under the mouse cursor,
+    # we could do lots of stuff but for the example just select.
+    if best_obj is not None:
+        best_obj.select = True
+        set_active(bpy.context, best_obj)
+
+
 def register():
     bpy.utils.register_module(__name__)
     bpy.types.Object.vox_empty = PointerProperty(type=VoxelEmpty_props)
@@ -209,10 +306,8 @@ def register():
 
 
 def unregister():
-    bpy.utils.unregister_class(VoxelMesh_obj_prop)
-    bpy.utils.unregister_class(VoxelEmpty_obj_prop)
+    bpy.utils.unregister_module(__name__)
     del bpy.types.Object.vox
-
 
 if __name__ == "__main__":
     register()
